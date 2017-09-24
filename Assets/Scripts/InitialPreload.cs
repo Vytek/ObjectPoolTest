1b184ce1697cbbf03b98406264d375bfff8f4b44
Enrico Speranza <enrico.speranza@gmail.com>
2017-09-24 18:03:31 +0200

    Initial commit

---
+++ Assets/Scripts/InitialPreload.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialPreload : MonoBehaviour {
+
+    public GameObject PrefabCube;
+    public GameObject PrefabSphere;
+
+    void Start()
+    {
+        this.PreloadGameObjectsIntoPool();
+    }
+
+    private void PreloadGameObjectsIntoPool()
+    {
+        GameObjectPooler.Instance.PreloadGameObject(PrefabCube);
+        GameObjectPooler.Instance.PreloadGameObject(PrefabSphere);
+    }
+}
